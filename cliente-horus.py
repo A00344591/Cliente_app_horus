ff2b630b53afeaa97cbcb7d97d09d3e4b29d8514
paok <2001.paok@gmail.co>
2017-07-06 04:08:30 -0500

    Renewed basic interface

--- cliente-horus.py
+++ cliente-horus.py
@@ -1,13 +1,24 @@
-#import requests
+print("Welcome to the Horus Manager app!!!")
+print("Type the command you want to execute or type 'help' if you lost")
 
-inp = ""
+def execute_shell():
+    while True:
+        inp = input(">>>")
+        inp = inp.lower()
+        Actions_description = ["Add: add an item to the database", "Remove: delete an item from the database ",
+                        "View: see information about an item", "Exit: exit shell"]
 
-while True:
-    inp = input(">>>")
+        Action_list = ["add", "remove", "exit", "view"]
+        if inp == "help":
+            print("The actions are: ")
+            for action in Actions_description:
+                print(action)
+        if inp not in Action_list and inp != "" and inp !="exit":
+            print("Not a valid command.")
 
-    if inp == "Display commands":
+        if inp=="exit":
+            break
 
-        Actions_list = ["Add", "Remove", "View"]
-        for action in Actions_list:
-            print(action)
 
+
+execute_shell()
