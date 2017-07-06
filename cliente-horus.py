a0408d738fa222392996d206b129155f66bc2440
Emiliano <A00344591>
2017-07-06 14:41:13 -0500

    first ideas of the app

--- cliente-horus.py
+++ cliente-horus.py
@@ -1,7 +1,13 @@
 print("Welcome to the Horus Manager app!")
-print("Type the command you want to execute or type 'help' if you lost")
+
+def users():
+    global user
+    user = input("User:")
+    global  passw
+    passw = input("Password:")
 
 def execute_shell():
+    print("Type the command you want to execute or type 'help' if you lost")
     while True:
         inp = input(">>>")
         inp = inp.lower()
@@ -18,7 +24,15 @@ def execute_shell():
 
         if inp=="exit":
             break
+def users2():
+    x_count = 0
+    while x_count < 3:
+        users()
+        if user == "HORUS6348" and passw == "double_sided_tape":
+            execute_shell()
+        else:
+            x_count += 1
+            print("Error de usuario... Vuelve a intentar...")
+            print("Te quedan " +str( 3-x_count ) + " intentos")
 
-
-
-execute_shell()
+users2()
